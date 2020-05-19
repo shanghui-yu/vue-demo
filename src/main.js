287ef485e221ec2e66a4700ee7097329e93ce87a
余尚辉 <1150322487@qq.com>
2020-05-19 17:48:44 +0800

    feat:安装ant desin

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Antd from 'ant-design-vue';
 import App from './App'
+import 'ant-design-vue/dist/antd.css';
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-
+Vue.use(Antd);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
